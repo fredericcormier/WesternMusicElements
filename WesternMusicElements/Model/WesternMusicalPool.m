7174a51dbcb58c7b3bb29e3c4cead91925e059a6
Frederic Cormier <tenstrings@me.com>
2012-03-05 21:27:10 +0100

    Tidyup

--- WesternMusicElements/Model/WesternMusicalPool.m
+++ WesternMusicElements/Model/WesternMusicalPool.m
@@ -79,8 +79,8 @@ static WesternMusicalPool *pool;
      :minor_pentatonic =>    [0,3,5,7,10,12]
      
      }*/
-    int CChromaticScaleValues[13] = {0,1,2,3,4,5,6,7,8,9,10,11,12};
-    int CMajorScaleValues[8] = {0,2,4,5,7,9,11,12};
+    int CChromaticScaleValues[13]   = {0,1,2,3,4,5,6,7,8,9,10,11,12};
+    int CMajorScaleValues[8]        = {0,2,4,5,7,9,11,12};
     int CNaturalMinorScaleValues[8] = {0,2,3,5,7,8,10,12};
     
     

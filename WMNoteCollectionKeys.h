6b4248fba79d34c4360c8f69d2f594cd1c9de584
Frederic Cormier <tenstrings@me.com>
2012-03-16 18:22:55 +0100

    adding more files

---
+++ WMNoteCollectionKeys.h
@@ -0,0 +1,56 @@
+//
+//  WMData.h
+//  WesternMusicElements
+//
+//  Created by Cormier Frederic on 06/03/12.
+//  Copyright (c) 2012 International MicrOondes. All rights reserved.
+//
+
+typedef NSString WMChordType;
+
+static WMChordType *WMChordTypeSeventhSus4 = @"Seventh sus4"
+static WMChordType *WMChordTypeAugmented7 = @"Augmented 7"
+static WMChordType *WMChordTypeNinth = @"Ninth"
+static WMChordType *WMChordTypeMinor6 = @"Minor 6"
+static WMChordType *WMChordTypeMajor13 = @"Major 13"
+static WMChordType *WMChordTypeMinor7 = @"Minor 7"
+static WMChordType *WMChordTypeMinor = @"Minor"
+static WMChordType *WMChordTypeMajor69 = @"Major 69"
+static WMChordType *WMChordTypeHalfDiminished7 = @"Half Diminished 7"
+static WMChordType *WMChordTypeThirteenth = @"Thirteenth"
+static WMChordType *WMChordTypeMinor11 = @"Minor 11"
+static WMChordType *WMChordTypeEleventh = @"Eleventh"
+static WMChordType *WMChordTypeDominant7 = @"Dominant 7"
+static WMChordType *WMChordTypeMinor9 = @"Minor 9"
+static WMChordType *WMChordTypeMinor13 = @"Minor 13"
+static WMChordType *WMChordTypeSus4 = @"Sus4"
+static WMChordType *WMChordTypeMinor69 = @"Minor 69"
+static WMChordType *WMChordTypeMajor = @"Major"
+static WMChordType *WMChordTypeMajor6 = @"Major 6"
+static WMChordType *WMChordTypeMinorMajor = @"Minor major"
+static WMChordType *WMChordTypeAugmented = @"Augmented"
+static WMChordType *WMChordTypeDiminished = @"Diminished"
+static WMChordType *WMChordTypeMajor7 = @"Major 7"
+static WMChordType *WMChordTypeDiminished7 = @"Diminished 7"
+static WMChordType *WMChordTypeMajor11 = @"Major 11"
+static WMChordType *WMChordTypeMajor9 = @"Major 9"
+
+typedef NSString WMScaleMode;
+
+static WMScaleMode *WMScaleModeAeolian = @"Aeolian"
+static WMScaleMode *WMScaleModeHarmonicMinor = @"Harmonic Minor"
+static WMScaleMode *WMScaleModeMelodicMinor = @"Melodic Minor"
+static WMScaleMode *WMScaleModeChromatic = @"Chromatic"
+static WMScaleMode *WMScaleModeMajorBebop = @"Major Bebop"
+static WMScaleMode *WMScaleModeMajorPentatonic = @"Major Pentatonic"
+static WMScaleMode *WMScaleModeGypsyMinor = @"Gypsy Minor"
+static WMScaleMode *WMScaleModeMajor = @"Major"
+static WMScaleMode *WMScaleModeLocrian = @"Locrian"
+static WMScaleMode *WMScaleModeDorian = @"Dorian"
+static WMScaleMode *WMScaleModeMixolydian = @"Mixolydian"
+static WMScaleMode *WMScaleModePhrygian = @"Phrygian"
+static WMScaleMode *WMScaleModeIonian = @"Ionian"
+static WMScaleMode *WMScaleModeMinorPentatonic = @"Minor Pentatonic"
+static WMScaleMode *WMScaleModeWholeTone = @"Whole Tone"
+static WMScaleMode *WMScaleModeLydian = @"Lydian"
+static WMScaleMode *WMScaleModeNaturalMinor = @"Natural Minor"

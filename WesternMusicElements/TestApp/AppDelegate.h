43392e786b2dfc131ea1b6e053adbd55293a5b26
Frederic Cormier <tenstrings@me.com>
2012-03-19 09:16:20 +0100

    Added AppDelegate back

---
+++ WesternMusicElements/TestApp/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  WesternMusicElements
+//
+//  Created by Cormier Frederic on 05/03/12.
+//  Copyright (c) 2012 International MicrOondes. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
